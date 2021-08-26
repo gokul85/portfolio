142ec768bb263f21f9af97dd7153ea728790c358
GOKUL K <79007791+K-Gokul@users.noreply.github.com>
2021-08-26 23:13:22 +0530

    my first commit portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ export default function About() {
                 <div className="row">
                     <div className="col-lg-5">
                         <div className="left-image">
-                            <img src="https://res.cloudinary.com/dm7i7i9wg/image/upload/v1629968771/Projects/profile_wyazbh.jpg" alt="" />
+                            <img src="https://res.cloudinary.com/dm7i7i9wg/image/upload/v1629999440/Projects/image_xmxzlc.jpg" alt="" />
                         </div>
                     </div>
                     <div className="col-lg-7">
