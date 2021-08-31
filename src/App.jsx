972686a9083cdc71ee35bda4b5501d765952a1e5
GOKUL K <79007791+K-Gokul@users.noreply.github.com>
2021-08-31 10:43:00 +0530

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/About';
 import Banner from './components/Banner';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
@@ -20,6 +21,7 @@ function App() {
         <Timeline />
         <Contact />
       </div>
+      <Footer />
     </div>
   );
 }
